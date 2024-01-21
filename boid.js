050a0b594f3db72370a751e71d88f4c4469178b7
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-21 14:02:58 -0800

    add Daniel Shifflan's flocking base code
    https://github.com/CodingTrain/Coding-Challenges/tree/main/124_Flocking_Boids/P5

---
+++ boid.js
@@ -0,0 +1,133 @@
+// Flocking
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+
+class Boid {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.acceleration = createVector();
+    this.maxForce = 0.2;
+    this.maxSpeed = 5;
+  }
+
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+
+  align(boids) {
+    let perceptionRadius = 25;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.velocity);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  separation(boids) {
+    let perceptionRadius = 24;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        diff.div(d * d);
+        steering.add(diff);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  flock(boids, alignmentCoeff, cohesionCoeff, separationCoeff) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+
+    alignment.mult(alignmentCoeff);
+    cohesion.mult(cohesionCoeff);
+    separation.mult(separationCoeff);
+
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(separation);
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    strokeWeight(6);
+    stroke(255);
+    point(this.position.x, this.position.y);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+// Flocking
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+
+const debug = false;
+
+const defaults = {
+  alignmentCoeff: 1,
+  cohesionCoeff: 1,
+  separationCoeff: 1,
+};
+
+const flock = [];
+
+let alignSlider, cohesionSlider, separationSlider;
+
+function setup() {
+  createCanvas(600, 400);
+  if (debug) {
+    alignSlider = createSlider(0, 2, 1.5, 0.1);
+    cohesionSlider = createSlider(0, 2, 1, 0.1);
+    separationSlider = createSlider(0, 2, 2, 0.1);
+  }
+  for (let i = 0; i < 300; i++) {
+    flock.push(new Boid());
+  }
+}
+
+function draw() {
+  background(0);
+  for (let boid of flock) {
+    boid.edges();
+    const alignmentCoeff =
+      (alignSlider && allignSlider.value()) || defaults.alignmentCoeff;
+    const cohesionCoeff =
+      (cohesionSlider && cohesionSlider.value()) || defaults.cohesionCoeff;
+    const separationCoeff =
+      (separationSlider && separationSlider.value()) ||
+      defaults.separationCoeff;
+
+    boid.flock(flock, alignmentCoeff, cohesionCoeff, separationCoeff);
+    boid.update();
+    boid.show();
+  }
+
+  if (debug) {
+    let colors = [];
+    colorMode(HSB, 255);
+    for (let i = 0; i < flock.length; i += 3) {
+      let r = map(flock[i].velocity.mag(), 0, 5, 0, 255);
+      let g = map(flock[i + 1].velocity.mag(), 0, 5, 0, 255);
+      let b = map(flock[i + 2].velocity.mag(), 0, 5, 0, 255);
+      colors.push(color(r, g, b));
+    }
+
+    let w = width / colors.length;
+    let h = 100;
+    colors.sort((a, b) => hue(a) - hue(b));
+    for (let i = 0; i < colors.length; i++) {
+      let c = colors[i];
+      fill(c);
+      noStroke();
+      rect(i * w, height - h, w, h);
+    }
+  }
+}
